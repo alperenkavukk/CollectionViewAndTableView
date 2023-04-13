4bbb248078ccb06fb90af5a257b7879839c09e92
alperenkavukk <kavuk.67.76@gmail.com>
2023-04-13 22:07:12 +0300

    Initial Commit

---
+++ collectionViewAndTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionViewAndTableView
+//
+//  Created by Alperen Kavuk on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
